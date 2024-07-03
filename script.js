3f5ac68123115ae6e040da382982b4dddf77c497
Umar Siddiqui <umarsiddiqui3037@gmail.com>
2024-07-03 20:58:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,16 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(responsiveSpline);
   }
 });
+
+// Hotjar  installation
+
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5046368,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })
+    (window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
